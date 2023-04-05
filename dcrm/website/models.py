abc830836a29cca78636ebd22739adfa827c508c
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-04-05 10:53:37 +0300

    added and registrated model, filed some data

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 
-class Record(model.Model):
+class Record(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
-    first_name =
-    last_name = 
-    email = 
-    phone = 
-    address =
-    city = 
-    state = 
-    zipcode = 
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=55)
+    state = models.CharField(max_length=55)
+    zipcode = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return (f'{self.first_name} {self.last_name} {self.city}' )
     
