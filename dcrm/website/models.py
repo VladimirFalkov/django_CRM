b74e978322ef0de424290f11ca566ba9c07e4983
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-04-05 10:11:41 +0300

    created all for rigistration

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(model.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name =
+    last_name = 
+    email = 
+    phone = 
+    address =
+    city = 
+    state = 
+    zipcode = 
+    
